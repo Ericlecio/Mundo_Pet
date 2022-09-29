65424f1fb1725d4d1c2486c89939a3d7c2b7c362
Ericlecio <etma@discente.ifpe.edu.br>
2022-09-29 10:21:21 -0300

    Configurando dao(animal e consulta e usuario)

--- mundoPet/src/main/java/br/edu/ifpe/mundopet/dao/ConsultaDao.java
+++ mundoPet/src/main/java/br/edu/ifpe/mundopet/dao/ConsultaDao.java
@@ -1,5 +1,75 @@
 package br.edu.ifpe.mundopet.dao;
 
-public class ConsultaDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import br.edu.ifpe.mundopet.model.Consulta;
+
+public class ConsultaDao {
+	public void AdicionarConsulta(Consulta consulta) throws ClassNotFoundException, SQLException{
+		Connection connection = ConexaoMySQL.getConexaoMySQL();
+		String sql = "INSERT INTO `Consulta`"
+				+ "(`idusuario`,`idveterinario`, `nome`, `data_consulta`, `horario`)"
+				+ "VALUES(?, ?, ?, ?, ?)";
+		PreparedStatement stmt =  connection.prepareStatement(sql);
+
+		stmt.setInt(1, consulta.getIdUsuario());
+		stmt.setInt(2, consulta.getIdVeterimario());
+		stmt.setString(3, consulta.getNome());
+		stmt.setInt(4, consulta.getData_Consulta());
+		stmt.setInt(5, consulta.getHorario());
+
+		stmt.execute();
+		stmt.close();
+		connection.close();
+	}
+	public List<Consulta> ConsultarConsultas() throws ClassNotFoundException, SQLException{
+		Connection connection =  ConexaoMySQL.getConexaoMySQL();
+		String sql = "SELECT `idconsulta`, `idusuario`, `idveterinario`,`nome`, `data_consulta`, `horario` FROM `consulta`";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+
+		ResultSet resultSet = stmt.executeQuery();
+
+		List<Consulta> listaTodasConsulta = new ArrayList<Consulta>();
+
+		while(resultSet.next()) {
+
+			Consulta consulta = new Consulta();
+
+			int idConsulta = resultSet.getInt("idconsulta");
+			consulta.setIdconsulta(idConsulta);;
+			int idusuario = resultSet.getInt("idusuario");
+			consulta.setIdUsuario(idusuario);
+			int idveterinario = resultSet.getInt("idveterinario");
+			consulta.setIdVeterimario(idveterinario);
+
+			consulta.setNome(resultSet.getString(3));;
+			consulta.setData_Consulta(resultSet.getInt(3));
+			consulta.setHorario(resultSet.getInt(2));
+
+			listaTodasConsulta.add(consulta);
+		}
+		stmt.close();
+		connection.close();
+
+		return listaTodasConsulta;
+	}
+
+
+	public void DeletarConsulta( int idconsulta) throws ClassNotFoundException, SQLException {
+		Connection connection = ConexaoMySQL.getConexaoMySQL();
+		String sql = "DELETE FROM `consulta` WHERE `idConsulta`=?";
+		PreparedStatement stmt = connection.prepareStatement(sql);
+		stmt.setInt(2, idconsulta);	
+
+		stmt.executeUpdate();
+		stmt.close();
+		connection.close();
+
+
+	}
 }
--- mundoPet/src/main/java/br/edu/ifpe/mundopet/model/Consulta.java
+++ mundoPet/src/main/java/br/edu/ifpe/mundopet/model/Consulta.java
@@ -2,22 +2,41 @@ package br.edu.ifpe.mundopet.model;
 
 public class Consulta {
 	private int idconsulta;
+	private int idUsuario;
+	private int idVeterimario;
 	private String nome;
 	private int data_Consulta;
 	private int horario;
 	
-	public Consulta(int idconsulta, String nome, int data_Consulta, int horario) {
+	public Consulta(int idconsulta, int idUsuario, int idVeterimario, String nome, int data_Consulta, int horario) {
 		super();
 		this.idconsulta = idconsulta;
+		this.idUsuario = idUsuario;
+		this.idVeterimario = idVeterimario;	
 		this.nome = nome;
 		this.data_Consulta = data_Consulta;
 		this.horario = horario;
+	}
+	public Consulta() {
+		// TODO Auto-generated constructor stub
 	}
 	public int getIdconsulta() {
 		return idconsulta;
 	}
 	public void setIdconsulta(int idconsulta) {
 		this.idconsulta = idconsulta;
+	}
+	public int getIdUsuario() {
+		return idUsuario;
+	}
+	public void setIdUsuario(int idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+	public int getIdVeterimario() {
+		return idVeterimario;
+	}
+	public void setIdVeterimario(int idVeterimario) {
+		this.idVeterimario = idVeterimario;
 	}
 	public String getNome() {
 		return nome;
@@ -37,6 +56,8 @@ public class Consulta {
 	public void setHorario(int horario) {
 		this.horario = horario;
 	}
+	
+	
 	
 	
 
