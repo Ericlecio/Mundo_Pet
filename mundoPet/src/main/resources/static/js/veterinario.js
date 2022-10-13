377cf16fca8dce831978e83c26f5f327781a245b
Ericlecio <etma@discente.ifpe.edu.br>
2022-10-13 08:58:00 -0300

    Atualizar

---
+++ mundoPet/src/main/resources/static/js/veterinario.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+        $('#dataNascimento').mask('00/00/0000')
+   })
+   
+    $(document).ready(function () { 
+        var $cpf = $("#cpf");
+        $cpf.mask('000.000.000-00');
+    });
