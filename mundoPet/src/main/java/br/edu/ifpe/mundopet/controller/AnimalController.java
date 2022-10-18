9bcc03b1cf64f36a94766b2436ee8a7977157806
vjarruda <arrudavini25@gmail.com>
2022-10-18 09:18:31 -0300

    atualiza arquivos animal

--- mundoPet/src/main/java/br/edu/ifpe/mundopet/controller/AnimalController.java
+++ mundoPet/src/main/java/br/edu/ifpe/mundopet/controller/AnimalController.java
@@ -3,30 +3,29 @@ package br.edu.ifpe.mundopet.controller;
 import java.sql.SQLException;
 import java.util.List;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifpe.mundopet.dao.AnimalDao;
 import br.edu.ifpe.mundopet.model.Animal;
+import br.edu.ifpe.mundopet.model.Veterinario;
 
 @Controller
 public class AnimalController {
-	
-//	@PostMapping("/animal")
-//	public String createAnimal() {
-//		return "redirect/animal";
-//	}
-	
+	@Autowired
+	AnimalDao animaldao;
+
 	@GetMapping("/animais")
 	public ModelAndView animal() {
-		
 		AnimalDao animaldao = new AnimalDao();
-		List<Animal> animais = null;
+		List<Animal> animal = null;
 		try {
-			animais = animaldao.ConsultarTodosAnimais();
+			animal = animaldao.ConsultarTodosAnimais();
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -34,10 +33,28 @@ public class AnimalController {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		ModelAndView mv = new ModelAndView("Animal/animal");
-		mv.addObject("animais",animais);
+		mv.addObject("animais",animal);
+		return mv;
+	}
+	@PostMapping("/animais")
+	public ModelAndView createAnimal(@Validated Animal animal, BindingResult bindingResults){
+		if(bindingResults.hasErrors()) {
+			ModelAndView mv = new ModelAndView("Animal/newAnimal");
+			mv.addObject("Animal", animal);
+			return mv;
+		}
+		try {
+			animaldao.AdicionarAnimal(animal);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return new ModelAndView("redirect:/animais") ;
+	}
+	@GetMapping("Animal/newAnimal")
+	public ModelAndView newAnimal() { 
+		ModelAndView mv = new ModelAndView("Animal/newAnimal");
 		return mv;
 	}
 }
-
--- mundoPet/src/main/java/br/edu/ifpe/mundopet/model/Animal.java
+++ mundoPet/src/main/java/br/edu/ifpe/mundopet/model/Animal.java
@@ -57,5 +57,4 @@ public class Animal {
 		this.sexo = sexo;
 	}
 	
-
 }
