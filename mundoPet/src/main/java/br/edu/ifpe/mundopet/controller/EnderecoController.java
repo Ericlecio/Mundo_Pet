1192fa923ca21c80ac5e5b3ea5a506709208e54f
Ericlecio <etma@discente.ifpe.edu.com.br>
2022-10-20 13:22:14 -0300

    Criação das telas de endereco

--- mundoPet/src/main/java/br/edu/ifpe/mundopet/controller/EnderecoController.java
+++ mundoPet/src/main/java/br/edu/ifpe/mundopet/controller/EnderecoController.java
@@ -1,5 +1,63 @@
 package br.edu.ifpe.mundopet.controller;
 
-public class EnderecoController {
+import java.sql.SQLException;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifpe.mundopet.dao.EnderecoDao;
+import br.edu.ifpe.mundopet.model.Endereco;
+import br.edu.ifpe.mundopet.model.Usuario;
+
+public class EnderecoController {
+	@Autowired
+	EnderecoDao enderecodao;
+
+	@GetMapping("/lista/enderecos")
+	public ModelAndView endereco() {
+		
+		EnderecoDao enderecodao = new EnderecoDao();
+		List<Endereco> endereco = null;
+		try {
+			endereco = enderecodao.ConsultarTodosEnderecos();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		ModelAndView mv = new ModelAndView("Endereco/endereco");
+		mv.addObject("enderecos",endereco);
+		return mv;
+	}
+	@PostMapping("/lista/enderecos")
+	public ModelAndView createEndereco(@Validated Endereco endereco, BindingResult bindingResults){
+		if(bindingResults.hasErrors()) {
+			ModelAndView mv = new ModelAndView("Endereco/cadastroEndereco");
+			mv.addObject("Endereco", endereco);
+			return mv;
+		}
+		try {
+			//mock
+			Usuario usuario =new Usuario();
+			usuario.setNome("fulano");
+			endereco.setIdUsuario(0);
+			enderecodao.AdicionarEndereco(endereco);
+		} catch (ClassNotFoundException | SQLException e) {
+			e.printStackTrace();
+		}
+		return new ModelAndView("redirect:/lista/enderecos") ;
+	}
+	@GetMapping("Endereco/cadastroEndereco")
+	public ModelAndView novoEndereco() { 
+		ModelAndView mv = new ModelAndView("Endereco/cadastroEndereco");
+		return mv;
+	}
 }
