0a569c51bfb4afb4366026e7902d8008c974bfe6
vjarruda <arrudavini25@gmail.com>
2022-11-08 09:53:18 -0300

    atualiza arquivos de consulta

--- mundoPet/src/main/java/br/edu/ifpe/mundopet/dao/ConsultaDao.java
+++ mundoPet/src/main/java/br/edu/ifpe/mundopet/dao/ConsultaDao.java
@@ -10,7 +10,10 @@ import java.util.List;
 
 import org.springframework.stereotype.Repository;
 
+import br.edu.ifpe.mundopet.model.Animal;
 import br.edu.ifpe.mundopet.model.Consulta;
+import br.edu.ifpe.mundopet.model.Usuario;
+import br.edu.ifpe.mundopet.model.Veterinario;
 
 @Repository
 public class ConsultaDao {
@@ -32,7 +35,7 @@ public class ConsultaDao {
 
 	public List<Consulta> ConsultarConsultas() throws ClassNotFoundException, SQLException {
 		Connection connection = ConexaoMySQL.getConexaoMySQL();
-		String sql = "SELECT c.idconsulta, u.nome, c.nome, v.nome, c.data_consulta, c.horario from consulta as a INNER JOIN usuario as u on a.idusuario = u.idusuario; veterinario as v on.idveterinario = v.idveterinario";
+		String sql = "SELECT c.idconsulta, u.nome, a.nome, v.nome, c.nome, c.data_consulta, c.horario from consulta as c INNER JOIN usuario as u on c.idusuario = u.idusuario inner join veterinario as v on c.idveterinario = v.idveterinario inner join animal as a on c.idanimal = a.idanimal;";
 		PreparedStatement stmt = connection.prepareStatement(sql);
 
 		ResultSet resultSet = stmt.executeQuery();
@@ -42,16 +45,22 @@ public class ConsultaDao {
 		while (resultSet.next()) {
 
 			Consulta consulta = new Consulta();
-
+			Usuario usuario = new Usuario();
+			Animal animal = new Animal();
+			Veterinario veterinario = new Veterinario();
+			
 			int idConsulta = resultSet.getInt("idconsulta");
 			consulta.setIdconsulta(idConsulta);
-			;
-			int idusuario = resultSet.getInt("idusuario");
-			int idveterinario = resultSet.getInt("idveterinario");
-			consulta.setNome(resultSet.getString(3));
-			;
-			consulta.setData_Consulta(new java.util.Date(resultSet.getDate(3).getTime()));
-			consulta.setHorario(resultSet.getInt(2));
+			usuario.setNome(resultSet.getString(2));
+			animal.setNome(resultSet.getString(3));
+			veterinario.setNome(resultSet.getString(4));
+			consulta.setNome(resultSet.getString(5));
+			consulta.setData_Consulta(new java.util.Date(resultSet.getDate(6).getTime()));
+			consulta.setHorario(resultSet.getInt(7));
+			
+			consulta.setAnimal(animal);
+			consulta.setUsuario(usuario);
+			consulta.setVeterinario(veterinario);
 
 			listaTodasConsulta.add(consulta);
 		}
--- mundoPet/src/main/java/br/edu/ifpe/mundopet/model/Consulta.java
+++ mundoPet/src/main/java/br/edu/ifpe/mundopet/model/Consulta.java
@@ -10,79 +10,60 @@ public class Consulta {
 
 	private Usuario usuario;
 	private Veterinario veterinario;
-	private Consulta consulta;
+	private Animal animal;
 
-
-	public Consulta(int idconsulta, String nome, Date data_Consulta, int horario, Usuario usuario,
-			Veterinario veterinario, Consulta consulta ) {
+	public Consulta(int idconsulta, String nome, Date data_Consulta, int horario) {
 		super();
 		this.idconsulta = idconsulta;
 		this.nome = nome;
 		this.data_Consulta = data_Consulta;
 		this.horario = horario;
-		this.usuario = usuario;
-		this.veterinario = veterinario;
-		this.consulta = consulta;
+		
 	}
-
-	public Consulta getConsulta() {
-		return consulta;
-	}
-
-	public void setConsulta(Consulta consulta) {
-		this.consulta = consulta;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	public Veterinario getVeterinario() {
-		return veterinario;
-	}
-
-	public void setVeterinario(Veterinario veterinario) {
-		this.veterinario = veterinario;
-	}
-
-	public Consulta() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public int getIdconsulta() {
 		return idconsulta;
 	}
-
 	public void setIdconsulta(int idconsulta) {
 		this.idconsulta = idconsulta;
 	}
-
 	public String getNome() {
 		return nome;
 	}
-
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
 	public Date getData_Consulta() {
 		return data_Consulta;
 	}
-
 	public void setData_Consulta(Date data_Consulta) {
 		this.data_Consulta = data_Consulta;
 	}
-
 	public int getHorario() {
 		return horario;
 	}
-
 	public void setHorario(int horario) {
 		this.horario = horario;
 	}
-
+	public Usuario getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	public Veterinario getVeterinario() {
+		return veterinario;
+	}
+	public void setVeterinario(Veterinario veterinario) {
+		this.veterinario = veterinario;
+	}
+	public Animal getAnimal() {
+		return animal;
+	}
+	public void setAnimal(Animal animal) {
+		this.animal = animal;
+	}
+	public Consulta() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 }
