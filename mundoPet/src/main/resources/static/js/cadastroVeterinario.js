47424e381f7cd50519d52a7b5f06a353b2f67bd9
Ericlecio <etma@discente.ifpe.edu.com.br>
2022-12-07 10:10:22 -0300

    Atualizando script

--- mundoPet/src/main/resources/static/js/cadastroVeterinario.js
+++ mundoPet/src/main/resources/static/js/cadastroVeterinario.js
@@ -1,36 +1,46 @@
 
-$(document).ready(function () {
+$(document).ready(function() {
 	$('#dataNascimento').mask('00/00/0000')
 })
 
-$(document).ready(function () {
+$(document).ready(function() {
 	var $cpf = $("#cpf");
 	$cpf.mask('000.000.000-00');
 })
 
-$(document).ready(function () {
+$(document).ready(function() {
 	var $crmv = $("#crmv");
 	$crmv.mask('000000000');
 })
 
-$("#nome").on("input", function(){
-  var regexp = /[^a-zA-Z]/g;
-  if(this.value.match(regexp)){
-    $(this).val(this.value.replace(regexp,''));
-  }
-})
-
 $(document).ready(function() {
-  $(document).on('keypress', '#senha', function(e){
-     return !(e.keyCode == 32);
-  });
+	$(document).on('keypress', '#senha', function(e) {
+		return !(e.keyCode == 32);
+	});
 });
 
-function mostrarsenha(){
-		var tipo = document.getElementById("senha");
-		if(tipo.type =="password"){
-			tipo.type = "text";
-		}else{
-			tipo.type = "password"
-		}
+function mostrarsenha() {
+	var tipo = document.getElementById("senha");
+	if (tipo.type == "password") {
+		tipo.type = "text";
+	} else {
+		tipo.type = "password"
 	}
+}
+
+
+$(function() {
+	$("#dataNascimento").datepicker({
+		changeMonth: true,
+		changeYear: true, 	
+		yearRange: "1950:2015",
+		dateFormat:"dd/mm/yy",
+		showOtherMonths:true,
+		selectOherMonths:true,
+        dayNames: ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado','Domingo'],
+        dayNamesMin: ['D','S','T','Q','Q','S','S','D'],
+        dayNamesShort: ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb','Dom'],
+        monthNames: ['Janeiro','Fevereiro','Março','Abril','Maio','Junho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        monthNamesShort: ['Jan','Fev','Mar','Abr','Mai','Jun','Jul','Ago','Set','Out','Nov','Dez']
+	});
+});
